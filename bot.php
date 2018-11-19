67c807d20f80fd0213ffeb592997be0554442d5e
nightperson13 <45182837+nightperson13@users.noreply.github.com>
2018-11-20 02:00:06 +0500

    Create bot.php

---
+++ bot.php
@@ -0,0 +1,16 @@
+$token = "781982351:AAFSaQXvZ4bfSJvaOhNfoWbQcWJHh7yomJk";
+$bot = new \TelegramBot\Api\Client($token);
+// команда для start
+$bot->command('start', function ($message) use ($bot) {
+    $answer = 'Добро пожаловать!';
+    $bot->sendMessage($message->getChat()->getId(), $answer);
+});
+
+// команда для помощи
+$bot->command('help', function ($message) use ($bot) {
+    $answer = 'Команды:
+/help - вывод справки';
+    $bot->sendMessage($message->getChat()->getId(), $answer);
+});
+
+$bot->run();
